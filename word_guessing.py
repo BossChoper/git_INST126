91c2a88fe1e6f9721280995953c9e17f74e00c51
BossC <martinkonteh2@gmail.com>
2024-04-09 16:36:39 -0400

    added print statements

--- word_guessing.py
+++ word_guessing.py
@@ -1,2 +1,9 @@
 import os
 import random
+
+print("Welcome to my Word Guessing Game!\n:")
+
+# get letter guess from user
+letter_guess = input("Guess a letter:]n")
+
+print(f"You guessed the letter '{letter_guess}")
