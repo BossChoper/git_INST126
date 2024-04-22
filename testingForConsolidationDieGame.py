9025e43611e720e43a130bcde3bd65b5ef0253ec
BossC <martinkonteh2@gmail.com>
2024-04-22 13:32:09 -0400

    updated code with more features related to for loops

--- testingForConsolidationDieGame.py
+++ testingForConsolidationDieGame.py
@@ -3,6 +3,7 @@ import random
 
 practice_results = []
 practice_dictionary = {1: 0, 2 : 0}
+all_practice_results = {1 : 0}
 two_sided_die = (1, 2)
 
 #turn 1 for player 1
@@ -58,28 +59,52 @@ print(practice_results)
 
 #add up total points for turn scoring
 total_points = 0
+
 #adds all numbers together in current roll results
-for int in practice_results:
-     total_points += int
-print(total_points)
+def addUpPoints():
+    for int in practice_results:
+        total_points += int
+    print(total_points)
 
 #goal to reach to win game for one player
 limit_score = 50
 
 #checks if any players rearched 50 points or game is over five turns
+#needs to be finished
 def isGameOver():
     if total_points == limit_score:
          print("Game Over")
 
 #shows current score for all players when called
+#needs to be finished
 def displayScore():
      print(practice_results)
 
 #keeps track of who has highest score
+#needs to be finished
 def highScore():
      if total_points > 1:
           print(total_points)
 
 #changes number of sides to roll
+#needs to be finished
 def changeDieSides():
      two_sided_die
+
+#testing player dictionary
+player_data = {"Player 1": (0, 1, 2),
+               "Player 2": (0, 1, 2)}
+player_data["Player 1"]
+player_data.keys()
+
+#print each player in the list one by one
+def printAllPlayers():
+    for player in player_data.keys():
+     print(player)
+
+#print out each player and their associated items
+def printAllPlayerInfo():
+    for player, points in player_data.items():
+     side1Points, side2Points, side3Points = points
+     print(f"{player} got {side1Points} for one side, {side2Points} for second side, {side3Points} for third side")
+     print(player)
